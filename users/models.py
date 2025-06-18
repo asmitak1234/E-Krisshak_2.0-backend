7097dd043114987cfcfe958f8ae8df0e3b3eeca5
ASMITA KUMARI <166974264+asmitak1234@users.noreply.github.com>
2025-06-18 15:26:46 +0530

    deployment

--- users/models.py
+++ users/models.py
@@ -32,7 +32,7 @@ class CustomUserManager(BaseUserManager):
         user.save()
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
@@ -42,7 +42,7 @@ class CustomUserManager(BaseUserManager):
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(username, email, password, **extra_fields)
     
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
