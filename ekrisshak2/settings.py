64f1d1e8c4c85b51efae48bbf362ab7461bd6081
ASMITA KUMARI <166974264+asmitak1234@users.noreply.github.com>
2025-06-21 15:16:03 +0530

    auth

--- ekrisshak2/settings.py
+++ ekrisshak2/settings.py
@@ -138,11 +138,15 @@ DATABASES = {
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES':[
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
+
 
 AUTH_USER_MODEL= 'users.CustomUser'
 
