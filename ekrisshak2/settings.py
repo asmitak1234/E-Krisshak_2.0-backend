a014f6c4bac7ddbd4a0ffedb626212d3405c2098
ASMITA KUMARI <166974264+asmitak1234@users.noreply.github.com>
2025-06-22 16:01:47 +0530

    user

--- ekrisshak2/settings.py
+++ ekrisshak2/settings.py
@@ -69,10 +69,10 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
