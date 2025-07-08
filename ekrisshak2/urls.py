95c5c92284d16f589be10706858365719d2e2106
ASMITA KUMARI <166974264+asmitak1234@users.noreply.github.com>
2025-07-08 18:45:12 +0530

    add

--- ekrisshak2/urls.py
+++ ekrisshak2/urls.py
@@ -29,11 +29,11 @@ urlpatterns = [
     path('api/notifications/', include('notifications.urls')),  # Live alerts & WebSocket updates
     path('api/payments/', include('payments.urls')),  # Razorpay integration & transactions
     path('api/core/', include('core.urls')),
-    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+    # path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
     path('api/search/', include('search.urls')),
 ]
 
 #     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
