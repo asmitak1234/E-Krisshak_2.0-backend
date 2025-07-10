ab088d1789c3b0d275e446b905cdcf998b912d3c
ASMITA KUMARI <166974264+asmitak1234@users.noreply.github.com>
2025-07-10 20:38:42 +0530

    soc

--- notifications/middleware.py
+++ notifications/middleware.py
@@ -3,27 +3,27 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.exceptions import AuthenticationFailed
 
 class TokenAuthMiddleware:
-    """Proper ASGI middleware for token-based WebSocket auth."""
+    """ASGI middleware that attaches `scope['user']` based on token query param."""
     def __init__(self, inner):
         self.inner = inner
 
     def __call__(self, scope):
-        return lambda receive, send: self.__acall__(scope, receive, send)
+        async def middleware(receive, send):
+            query_string = scope.get("query_string", b"").decode()
+            token = None
 
-    async def __acall__(self, scope, receive, send):
-        query_string = scope.get("query_string", b"").decode()
-        token = None
+            if "token=" in query_string:
+                token = query_string.split("token=")[-1].split("&")[0]
 
-        if "token=" in query_string:
-            token = query_string.split("token=")[-1].split("&")[0]
-
-        if token:
-            try:
-                user, _ = TokenAuthentication().authenticate_credentials(token.encode())
-                scope["user"] = user
-            except AuthenticationFailed:
+            if token:
+                try:
+                    user, _ = TokenAuthentication().authenticate_credentials(token.encode())
+                    scope["user"] = user
+                except AuthenticationFailed:
+                    scope["user"] = AnonymousUser()
+            else:
                 scope["user"] = AnonymousUser()
-        else:
-            scope["user"] = AnonymousUser()
 
-        return await self.inner(scope)(receive, send)
+            return await self.inner(scope)(receive, send)
+
+        return middleware
