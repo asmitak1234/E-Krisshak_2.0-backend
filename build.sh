8d2046d919e14b17ab352d338a90e62529dfa679
ASMITA KUMARI <166974264+asmitak1234@users.noreply.github.com>
2025-07-19 18:16:08 +0530

    deploy

--- build.sh
+++ build.sh
@@ -8,4 +8,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # Apply database migrations
-python manage.py migrate
+python manage.py migrate --noinput
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='asmita').exists() or User.objects.create_superuser('asmita', 'asmita2275@gmail.com', 'Asmita#132')" | python manage.py shell
+
