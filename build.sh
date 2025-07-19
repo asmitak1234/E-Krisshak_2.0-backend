7bf52996bca6bf0b207a68e2c0b833f04be890e7
ASMITA KUMARI <166974264+asmitak1234@users.noreply.github.com>
2025-07-19 18:38:03 +0530

    build

--- build.sh
+++ build.sh
@@ -8,13 +8,17 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # Apply database migrations
+python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 
 python manage.py shell << END
 from django.contrib.auth import get_user_model
 User = get_user_model()
-if not User.objects.filter(username='asmita').exists():
-    User.objects.create_superuser('asmita', 'asmita2275@gmail.com', 'Asmita#132')
+if not User.objects.filter(email='asmita2275@gmail.com').exists():
+    User.objects.create_superuser(
+        email='asmita2275@gmail.com',
+        password='Asmita#132',
+        name='Asmita'
+    )
 END
-
 
