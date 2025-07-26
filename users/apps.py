c5e3abcadf5f92f6f2914d61050cc4f4bf23ac2b
ASMITA KUMARI <166974264+asmitak1234@users.noreply.github.com>
2025-07-26 15:44:38 +0530

    load

--- users/apps.py
+++ users/apps.py
@@ -1,16 +1,5 @@
 from django.apps import AppConfig
-from django.core.management import call_command
-from django.db.utils import OperationalError
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
-
-    def ready(self):
-        try:
-            from .models import State
-            if not State.objects.exists():
-                call_command("load_data")
-                print("✅ State & District data loaded on startup")
-        except OperationalError:
-            print("⛔ DB not ready on first boot — skipping load")
