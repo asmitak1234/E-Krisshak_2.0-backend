0cca71e1722261095ef6b862f584774aa6657e18
ASMITA KUMARI <166974264+asmitak1234@users.noreply.github.com>
2025-07-28 10:14:40 +0530

    urls

--- ekrisshak2/urls.py
+++ ekrisshak2/urls.py
@@ -15,12 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
 
+def health_check(request):
+    return JsonResponse({"ok": True})
+
 urlpatterns = [
+    path("", health_check),   
     path('admin/', admin.site.urls),  
     path('api/users/', include('users.urls')),  # User authentication & role-based access
     path('api/contact/', include('contact.urls')),  # Contact messages & admin escalation
